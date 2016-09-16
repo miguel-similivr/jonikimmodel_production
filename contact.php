67541abb2d57e065c9edbda7a85b2771ed08e969
miguel-similivr <miguel@similivr.com>
2016-09-15 23:27:28 -0700

    simplified nav, php converted everything

--- contact.php
+++ contact.php
@@ -70,58 +70,13 @@ require_once('lib/phpmailer/class.pop3.php');
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Joni Kim</title>
+<?php require("nav/include_nav.php"); ?>
+<?php insertTitle(["title" => "Joni Kim - Vancouver Model"]); ?>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
-    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-    <!-- Nav -->
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">
-            <img class="img-responsive" src="images/joni_logo_001_stacked.png" alt="brand" style="border: none; width: 100%;" >
-          </a>
-        </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="home.html">Home</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li><a href="bio.html">Bio</a></li>
-            <li><a class="current" href="contact.php">Contact</a></li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav> <!-- /end of nav -->
+<script type="text/javascript">
+  var currentTab = document.getElementById("contact-nav");
+  currentTab.className = "current";
+</script>
 
     <!-- line break -->
     <hr>
